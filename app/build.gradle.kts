3adc7018bb80f0534a1892527818d25b7a4bc13f
Harsh Lashkarbhai Dhameliya <86516970+harshDhameliya6592@users.noreply.github.com>
2025-08-19 22:56:27 +0530

    1. First Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.gtrbx.gtrbxcounter034"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.gtrbx.gtrbxcounter034"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.flexbox)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.browser)
+
+    implementation(libs.androidscratchcard)
+
+    implementation(libs.androidx.lifecycle.process)
+    implementation(platform(libs.firebase.bom.v3281))
+    implementation(libs.firebase.database)
+
+    implementation(libs.gson)
+    implementation(libs.glide)
+}
