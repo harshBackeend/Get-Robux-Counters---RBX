8c36b4d49a91e410d60571cf82467844c3e95da5
Harsh Lashkarbhai Dhameliya <86516970+harshDhameliya6592@users.noreply.github.com>
2025-08-22 23:02:32 +0530

    1. All Fill Check And Done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.gtrbx.gtrbxcounter034"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.gtrbx.gtrbxcounter034"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
